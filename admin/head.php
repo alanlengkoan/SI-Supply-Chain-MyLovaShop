1710342e44bc17923a91c6609d1bee577caa1753
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-12-06 23:05:37 +0800

    update again

--- admin/head.php
+++ admin/head.php
@@ -1,7 +1,7 @@
 <?php
 // untuk koneksi
 session_start();
-include_once ('../db/koneksi.php');
+include_once('../db/koneksi.php');
 if ($_SESSION['inpuser'] == "") {
   echo "<script>
           window.alert('Anda Harus Masuk Dahulu !');
@@ -9,28 +9,29 @@ if ($_SESSION['inpuser'] == "") {
         </script>";
 }
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>SI Logistik Pergudangan</title>
 
-    <!-- Pemanggilan Bootstrap -->
-    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
+<head>
+  <meta charset="utf-8">
+  <title>Sistem Informasi Supply Chain</title>
 
-    <!-- Pemanggilan Icon dan Gambar -->
-    <link rel="stylesheet" href="../vendor/font-awesome/css/font-awesome.min.css">
+  <!-- Pemanggilan Bootstrap -->
+  <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
 
-    <!-- Pemanggilan DataTables -->
-    <link rel="stylesheet" href="../vendor/datatables.net-bs/css/dataTables.bootstrap.min.css">
-    <link rel="stylesheet" href="../vendor/datatables.net-buttons-bs/css/buttons.bootstrap.min.css">
-    <link rel="stylesheet" href="../vendor/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css">
-    <link rel="stylesheet" href="../vendor/datatables.net-responsive-bs/css/responsive.bootstrap.min.css">
-    <link rel="stylesheet" href="../vendor/datatables.net-scroller-bs/css/scroller.bootstrap.min.css">
+  <!-- Pemanggilan Icon dan Gambar -->
+  <link rel="stylesheet" href="../vendor/font-awesome/css/font-awesome.min.css">
 
-    <!-- Tema CSS -->
-    <link rel="stylesheet" href="../css/custom.min.css">
+  <!-- Pemanggilan DataTables -->
+  <link rel="stylesheet" href="../vendor/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="../vendor/datatables.net-buttons-bs/css/buttons.bootstrap.min.css">
+  <link rel="stylesheet" href="../vendor/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css">
+  <link rel="stylesheet" href="../vendor/datatables.net-responsive-bs/css/responsive.bootstrap.min.css">
+  <link rel="stylesheet" href="../vendor/datatables.net-scroller-bs/css/scroller.bootstrap.min.css">
 
-  </head>
+  <!-- Tema CSS -->
+  <link rel="stylesheet" href="../css/custom.min.css">
+
+</head>
